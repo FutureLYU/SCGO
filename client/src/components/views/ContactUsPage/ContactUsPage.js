6524a166722f834be8e58fb4566df93eac9e13b5
HaroldXu97 <harold_xu@outlook.com>
2020-07-20 12:36:30 -0700

    update

---
+++ client/src/components/views/ContactUsPage/ContactUsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContactUsPage() {
+    return (
+        <div>
+            You can contact us by ...
+        </div>
+    )
+}
+
+export default ContactUsPage
